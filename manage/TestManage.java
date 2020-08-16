17072e0fd77dc8864d232df63687b8f01033766e
AiDatawxy <39855600+AiDatawxy@users.noreply.github.com>
2020-08-16 20:34:49 +0800

    Add files via upload

---
+++ manage/TestManage.java
@@ -0,0 +1,40 @@
+package manage;
+
+import java.util.*;
+import javafx.scene.layout.GridPane;
+import java.io.*;
+
+public class TestManage {
+	//========== Test Functions in Class manage.Manage ============//
+	
+	public static void main(String[] args) {
+		
+	/*
+	Manage.addRegisterInformation("wxy1", "1", "nick1");	
+	Manage.addRegisterInformation("wxy2", "12", "nick2");	
+	Manage.addRegisterInformation("wxy3", "123", "nick3");	
+	Manage.addRegisterInformation("wxy4", "1234", "nick4");	
+	*/
+		
+	/*
+	String[] user = new String[3];
+	user = Manage.searchInformation("wxy6");
+	for(String s: user)
+		System.out.print(s + " ");
+	*/
+		
+	/*
+	Manage.setRegisterInformation("wxy2", "xinmimamima", "xinnickaname");
+	*/
+		
+	/*
+	 ArrayList<String> usrnames = Manage.getUsernamesArrayList();
+	 for(String s: usrnames)
+		 System.out.print(s + " ");
+	 */
+		
+		
+	}
+	
+
+}
